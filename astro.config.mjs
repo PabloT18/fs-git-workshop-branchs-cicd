36814dcbca7da6d217a79add49ab722281917ee6
PabloTorres <ptorresp@ups.edu.ec>
2026-06-09 00:55:31 -0500

    fix: configura base path para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://pablot18.github.io",
+    base: "/fs-git-workshop-branchs-cicd/",
+});
